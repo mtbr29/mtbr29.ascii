67dca513a7d7ba27c1c52acccb1e367907d5699d
mtbr29 <157277351+mtbr29@users.noreply.github.com>
2025-04-02 15:14:27 +0200

    Update frames.go

--- frames/frames.go
+++ frames/frames.go
@@ -63,5 +63,5 @@ var FrameMap = map[string]FrameType{
 	"earth":           Earth,
 	"kitty":           Kitty,
 	"india":           India,
-	"brittany":        Brittany
+	"brittany":        Brittany,
 }
